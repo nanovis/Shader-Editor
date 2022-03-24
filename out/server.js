8a365ad871b882566147798918025775730235e1
donggang jia <752363183@qq.com>
2022-03-24 13:45:19 +0800

    add trial token

--- out/server.js
+++ out/server.js
@@ -4,7 +4,7 @@ const bodyParser=require("body-parser")
 var hbs = require('hbs');
 var fs = require('fs')
 const app = express()
-const port = 8000
+const port = 8080
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(__dirname))
