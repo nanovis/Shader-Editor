08800bb7575df7af76413f12254954178678905c
donggang jia <752363183@qq.com>
2023-02-08 12:30:45 +0300

    update origin trial

--- out/assets/js/window.js
+++ out/assets/js/window.js
@@ -5,13 +5,13 @@ function reportWindowSize()
       var height=window.innerHeight;
       var new_height=45*screen.height/height;
       var new_width=45*screen.width/width;
-      if(new_width>70)
+      if(new_width>50)
       {
         var factor=new_width/new_height;
         new_width=45;
         new_height=new_width/factor;
       }
-      if(new_height>70)
+      if(new_height>50)
       {
         var factor=new_width/new_height;
         new_height=45;
